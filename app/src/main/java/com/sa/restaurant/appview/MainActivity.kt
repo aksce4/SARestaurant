1590ac08c91576fe67f189b93f83cc570691d85b
aksce3 <aksce3@gmail.com>
2018-07-23 18:46:01 +0530

    third commit

--- app/src/main/java/com/sa/restaurant/appview/MainActivity.kt
+++ app/src/main/java/com/sa/restaurant/appview/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.sa.restaurant.R
-import com.sa.restaurant.appview.splash.SplashActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,17 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        button.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(view: View?) {
-                splashhandle(view)
-            }
-
-        })
     }
 
-    private fun splashhandle(view: View?) {
-        var intent: Intent = Intent(this, SplashActivity::class.java)
-        startActivity(intent)
-    }
+
 }
--- app/src/main/java/com/sa/restaurant/appview/splash/SplashActivity.kt
+++ app/src/main/java/com/sa/restaurant/appview/splash/SplashActivity.kt
@@ -1,15 +1,33 @@
 package com.sa.restaurant.appview.splash
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.sa.restaurant.R
-import kotlinx.android.synthetic.main.activity_splash.view.*
+import com.sa.restaurant.appview.MainActivity
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        supportActionBar?.hide()
+        setContentView(R.layout.activity_splash_screen)
+
+        Thread(Runnable {
+            try {
+                val visibility = if (progress_bar.visibility == View.GONE) View.VISIBLE else View.GONE
+                progress_bar.visibility = visibility
+                Thread.sleep(5000)
+            }catch (e: InterruptedException){
+                Log.e("SplashActivity","Interrupt Occur $e")
+            }finally {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }).start()
     }
 
 }
