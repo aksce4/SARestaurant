84356923fdbfdf7c4513583f6e25418150011542
aksce3 <aksce3@gmail.com>
2018-07-25 19:14:56 +0530

    11th commit

--- app/src/main/java/com/sa/restaurant/appview/login/presenter/LoginPresenterImpl.kt
+++ app/src/main/java/com/sa/restaurant/appview/login/presenter/LoginPresenterImpl.kt
@@ -1,6 +1,7 @@
 package com.sa.restaurant.appview.login.presenter
 
 import android.app.Activity
+import android.widget.Toast
 import com.sa.restaurant.appview.login.LoginFragment
 import com.sa.restaurant.appview.roomdatabase.LoginTable
 import com.sa.restaurant.appview.roomdatabase.MyDatabase
@@ -14,7 +15,11 @@ class LoginPresenterImpl: LoginPresenter{
             var loginFragment: LoginFragment = LoginFragment()
             var username = result[0].username
             var password = result[0].password
-            loginFragment.
+            loginFragment.authUser(activity, username, password)
+            return true
+        }else{
+            Toast.makeText(activity, "Invalid Username or Password", Toast.LENGTH_SHORT).show()
+            return false
         }
     }
 
